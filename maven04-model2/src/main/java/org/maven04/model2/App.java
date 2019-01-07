f3f35ea25df6746b9baabb5acec093999330b4c5
zss01 <18239395309@163.com>
2019-01-07 10:00:14 +0800

    init project

---
+++ maven04-model2/src/main/java/org/maven04/model2/App.java
@@ -0,0 +1,13 @@
+package org.maven04.model2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
